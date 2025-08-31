a43532cd0a062cf2c365931bd0206140cc6ca697
Ritesh Kumar <riteshkr7720@gmail.com>
2025-08-31 17:34:27 +0530

    add portfolio

---
+++ portfolio_web.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PORTFOLIO</title>
+    <link rel="icon" type="image/x-icon" href="images/PicsArt_03-17-05.37.13.jpg">
+    <link rel="stylesheet" href="css/portfolio_web.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="navigation">
+        <div class="navlink">
+            <ul>
+                <li><a href="#main">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#service">Services</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <section class="sec" id="main">
+        <header>
+            <div class="toggles"><span></span></div>
+            <div class="logo"><span>P</span><span>ortfolio</span></div>
+            <ul>
+                <li><a href="#main">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#service">Services</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </ul>
+        </header>
+
+        <div class="imgbox">
+            <img src="images/self.png" alt="#" class="cover">
+        </div>
+
+        <div class="content">
+            <h2>Hi, I am <br><span>Ritesh Kumar</span><span>(Web Designer/Developer)</span></h2>
+            <p>This is my official portfolio website to show all my details and work experience.</p>
+            <a href="images/shubham.docx" download="shubham.docx">Download My CV</a>
+        </div>
+    </section>
+    <section class="about_me" id="about">
+        <div class="about_flex">
+            <div class="about_img"><img src="images/pexels-tony-schnagl-5586307.jpg" alt=""></div>
+            <div class="about_content">
+                <h1>About Me</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis natus a aut est magnam modi nihil
+                    iure impedit totam dolore! Ad provident optio magni dicta reiciendis rerum, earum nemo eaque
+                    quibusdam enim nobis minima repellendus, nihil aliquam nostrum labore sed omnis ullam consequatur
+                    eos vel deleniti asperiores eius mollitia. Inventore soluta velit et ut quam deleniti vero fugit
+                    asperiores dolorum!
+                </p>
+
+                <div class="allh4">
+                    <h4>Skills</h4>
+                    <h4>Experience</h4>
+                    <h4>Education</h4>
+                </div>
+
+                <div class="showSkills">
+                    <h5>UI/UX Design</h5>
+                    <p>Designing Web/App Interfaces</p>
+                    <h5>Web Development</h5>
+                    <p>Web App Development</p>
+                    <h5>App Development</h5>
+                    <p>Building Android/iOS Apps</p>
+                </div>
+
+                <div class="showExperience">
+                    <h5>2021-Current</h5>
+                    <p>UI/UX Design Training at ET Institute</p>
+                    <h5>2019-2021</h5>
+                    <p>Team Lead at StarApp LLC.</p>
+                    <h5>2017-2019</h5>
+                    <p>UI/UX Design Executive at Coin Digital Limited</p>
+                    <h5>2016-2017</h5>
+                    <p>Internship at ekart commerce</p>
+                </div>
+
+                <div class="showEducation">
+                    <h5>2023</h5>
+                    <p>Completed Web Designing/Development Program From OKSoft Solution, Saharanpur</p>
+                    <h5>2020</h5>
+                    <p>Completed B.Sc From CCS University, Meerut</p>
+                    <h5>2017</h5>
+                    <p>Intermediate From C.B.S.E Board With 70%</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="services" id="service">
+        <h1>My Services</h1>
+        <div class="services">
+            <div class="service html">
+                <h1>Web Design</h1>
+                <h3><span><i class="fa-brands fa-html5"></i></span><span><i class="fa-brands fa-css3-alt"></i></span><span><i class="fa-brands fa-js"></i></span></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam excepturi odio maiores voluptatum illo consequatur officiis ratione, tenetur beatae cupiditate.</p>
+                <a href="">Learn More</a>
+            </div>
+            <div class="service php">
+                <h1>Web Development</h1>
+                <h3><span><i class="fa fa-php" aria-hidden="true"></i></span></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam excepturi odio maiores voluptatum illo consequatur officiis ratione, tenetur beatae cupiditate.</p>
+                <a href="">Learn More</a>
+            </div>
+            <div class="service sql">
+                <h1>Databases</h1>
+                <h3><span><i class="fa fa-database" aria-hidden="true"></i></span><span><i class="fa fa-mysql" aria-hidden="true"></i></span></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam excepturi odio maiores voluptatum illo consequatur officiis ratione, tenetur beatae cupiditate.</p>
+                <a href="">Learn More</a>
+            </div>
+        </div>
+    </section>
+    <section class="mywork" id="work">
+        <h1>My Work</h1>
+        <div class="mywork">
+            <div class="work">
+                <img src="images/Screenshot_20230129_223045.png" alt="">
+                <div class="hover">
+                    <h4>Form With JS Validations</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, numquam? Cumque deleniti corporis aliquam ipsum quod dignissimos culpa veniam asperiores!</p>
+                    <a href="http://localhost/projects/validate_form.php">View</a>
+                </div>
+            </div>
+            <div class="work">
+                <img src="images/Screenshot_20230129_235038.png" alt="">
+                <div class="hover">
+                    <h4>Image Slider Made With SASS And JS</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, numquam? Cumque deleniti corporis aliquam ipsum quod dignissimos culpa veniam asperiores!</p>
+                    <a href="slider_sass.html">View</a>
+                </div>
+            </div>
+            <div class="work">
+                <img src="images/Screenshot_20230129_215234.png" alt="">
+                <div class="hover">
+                    <h4>JS Clock</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, numquam? Cumque deleniti corporis aliquam ipsum quod dignissimos culpa veniam asperiores!</p>
+                    <a href="clock.html">View</a>
+                </div>
+            </div>
+        </div>
+        <a href="">See More</a>
+    </section>
+    <section class="contact" id="contact">
+        <div class="contact_flex">
+            <div class="icons">
+                <h1>Contact Me</h1>
+                <p><i class="fa-sharp fa-solid fa-paper-plane"></i><span>riteshkr7720@gmail.com</span></p>
+                <p><i class="fa-solid fa-phone"></i><span>9548899651</span></p>
+                <ul>
+                    <li><a href="https://www.facebook.com/profile.php?id=100004120097489"><img src="images/fb.png" alt=""></a>
+                    </li>
+                    <li><a href="https://www.instagram.com/ritesh_rayash_77/"><img src="images/insta.png" alt=""></a></li>
+                    <li><a href="https://www.linkedin.com/in/ritesh-kumar-rayash-2627aa215/"><img src="images/unnamed.png"
+                                alt=""></a></li>
+                    <li><a href="https://github.com/RiteshRayash77"><img src="images/unnamed (1).png" alt=""></a></li>
+                </ul>
+                <a href="#" class="btn">Hire Me</a>
+            </div>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="subform">
+                <input type="text" placeholder="Your Name" name="name" id="fname">
+                <input type="email" placeholder="Your Email" name="email" id="femail">
+                <textarea id="fmessage" cols="30" rows="10" placeholder="Enter Your Message..." name="message"></textarea>
+                <span id="show_message" style="display: block;"></span>
+                <input type="button" name="submit" id="sub" value="Submit">
+            </form>
+        </div>
+        <p class="copyright"><i class="fa-regular fa-copyright"></i> Copyright 2023 Ritesh Kumar</p>
+    </section>
+    <div id="scrollto"><i class="fa-sharp fa-solid fa-arrow-up"></i></div>
+
+    <script
+  src="https://code.jquery.com/jquery-3.6.3.min.js"
+  integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+  crossorigin="anonymous"></script>
+    <script>
+        let menutoggle = document.querySelector(".toggles");
+        let navigation = document.querySelector(".navigation");
+        let sec = document.querySelector(".sec");
+        menutoggle.onclick = function () {
+            menutoggle.classList.toggle("active");
+            navigation.classList.toggle("active");
+            sec.classList.toggle("active");
+        }
+
+        let showSkills1 = document.querySelector('.allh4 h4:nth-child(1)');
+        let showSkills2 = document.querySelector('.allh4 h4:nth-child(2)');
+        let showSkills3 = document.querySelector('.allh4 h4:nth-child(3)');
+        let showSkills1div1 = document.querySelector('.about_content .showSkills');
+        let showSkills1div2 = document.querySelector('.about_content .showExperience');
+        let showSkills1div3 = document.querySelector('.about_content .showEducation');
+
+        const addClass = () => {
+            showSkills1div1.classList.remove('showSkills');
+            showSkills1.setAttribute('class','active');
+        };
+        addClass();
+
+        showSkills1.addEventListener('click', (event) => {
+            showSkills1div1.removeAttribute('class', 'showSkills');
+            showSkills1div2.setAttribute('class', 'showExperience');
+            showSkills1div3.setAttribute('class', 'showEducation');
+            event.target.setAttribute('class','active');
+            showSkills2.removeAttribute('class','active');
+            showSkills3.removeAttribute('class','active');
+        });
+        showSkills2.addEventListener('click', (event) => {
+            showSkills1div1.setAttribute('class', 'showSkills');
+            showSkills1div2.removeAttribute('class', 'showExperience');
+            showSkills1div3.setAttribute('class', 'showEducation');
+            event.target.setAttribute('class','active');
+            showSkills1.removeAttribute('class','active');
+            showSkills3.removeAttribute('class','active');
+        });
+        showSkills3.addEventListener('click', (event) => {
+            showSkills1div1.setAttribute('class', 'showSkills');
+            showSkills1div2.setAttribute('class', 'showExperience');
+            showSkills1div3.removeAttribute('class', 'showEducation');
+            event.target.setAttribute('class','active');
+            showSkills1.removeAttribute('class','active');
+            showSkills2.removeAttribute('class','active');
+        });
+
+        let navbuttons=document.querySelectorAll('.navigation .navlink ul li a');
+        navbuttons.forEach(element=>{
+            element.addEventListener('click',(event)=>{
+                navigation.classList.remove('active');
+                menutoggle.classList.remove('active');
+            });
+        });
+
+        let scroll_butt=document.querySelector('#scrollto');
+        scroll_butt.addEventListener('click',()=>{
+            window.scrollTo(0,0);
+        });
+        window.addEventListener('scroll',()=>{
+            if(window.scrollY>=1100){
+                scroll_butt.style.right='50px';
+            }else{
+                scroll_butt.style.right='-50px';
+            }
+        });
+    </script>
+    <script>
+        $(document).ready(function(){
+            $('#sub').on('click',function(e){
+                let name=$('#fname').val();
+                let email=$('#femail').val();
+                let message=$('#fmessage').val();
+                // $('#show_message').html(`Hi ${name} your message send successfully !`);
+                // $.ajax({
+                //     url : 'http://localhost/projects/delete.php',
+                //     type : "POST",
+                //     success : function(data){
+                //         $('#show_message').html(data);
+                //     }
+                // });
+                $.post(
+                    'http://localhost/projects/portfolio_data_save.php',
+                    $('#subform').serialize(),
+                    function(data){
+                        if(data==1){
+                            $('#subform').trigger('reset');
+                            $('#show_message').fadeIn();
+                            $('#show_message').html("<span style='text-align:center;color:#30d830;display:block;'>Hi "+name+" your message sent successfully!</span>");
+                            setTimeout(function(){
+                                $('#show_message').fadeOut();
+                            },5000);
+                        }else{
+                            $('#show_message').html("<span style='text-align:center;color:red;display:block;'>All fields are required!</span>")
+                            $('#show_message').fadeIn();
+                            setTimeout(function(){
+                                $('#show_message').fadeOut();
+                            },5000);
+                        }
+                    }
+                )
+            });
+        });
+    </script>
+</body>
+
+</html>
